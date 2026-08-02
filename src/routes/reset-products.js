133ce87055081494ebd5e850d5b303cb427ad785
anasikene10 <anasikene10@gmail.com>
2026-08-02 20:48:38 +0100

    Update reset-products.js

--- src/routes/reset-products.js
+++ src/routes/reset-products.js
@@ -16,14 +16,14 @@ router.get("/", async (req, res) => {
       { id: 2, name: "NY CAP BLACK", tag: "COTON TWILL", price: 170, stock: 20 },
       { id: 3, name: "LA CAP FULL BLACK", tag: "COTON TWILL", price: 170, stock: 20 },
       { id: 4, name: "LA CAP BLACK STARS", tag: "COTON TWILL", price: 170, stock: 20 },
-      { id: 5, name: "DAD HAT CREAM", tag: "COTON LAVÉ", price: 199, stock: 20 },
-      { id: 6, name: "DAD HAT NAVY BLUE", tag: "COTON LAVÉ", price: 199, stock: 20 },
-      { id: 7, name: "DAD HAT BURGUNDY", tag: "COTON LAVÉ", price: 199, stock: 20 },
-      { id: 8, name: "DAD HAT CAMEL BROWN", tag: "COTON LAVÉ", price: 199, stock: 20 },
-      { id: 9, name: "FITTED BLACK ÉDITION", tag: "LAINE MÉLANGÉE", price: 299, stock: 20 },
-      { id: 10, name: "FITTED GREEN ÉDITION", tag: "LAINE MÉLANGÉE", price: 299, stock: 20 },
-      { id: 11, name: "TRUCKER LAVENDER", tag: "MAILLE TECHNIQUE", price: 160, stock: 20 },
-      { id: 12, name: "TRUCKER LIGHT BLUE", tag: "MAILLE TECHNIQUE", price: 160, stock: 0 },
+      { id: 5, name: "LORO PIANA CREAM", tag: "COTON LAVÉ", price: 199, stock: 20 },
+      { id: 6, name: "LORO PIANA NAVY BLUE", tag: "COTON LAVÉ", price: 199, stock: 20 },
+      { id: 7, name: "LORO PIANA BURGUNDY", tag: "COTON LAVÉ", price: 199, stock: 20 },
+      { id: 8, name: "LORO PIANA CAMEL BROWN", tag: "COTON LAVÉ", price: 199, stock: 20 },
+      { id: 9, name: "GUCCI BLACK 1-1", tag: "LAINE MÉLANGÉE", price: 299, stock: 20 },
+      { id: 10, name: "GUCCI GREEN 1-1", tag: "LAINE MÉLANGÉE", price: 299, stock: 20 },
+      { id: 11, name: "POLO LAVENDER", tag: "MAILLE TECHNIQUE", price: 160, stock: 20 },
+      { id: 12, name: "POLO LIGHT BLUE", tag: "MAILLE TECHNIQUE", price: 160, stock: 0 },
     ];
 
     for (const p of products) {
